92df29c7ce805374caefa6597c4d95f1169792b1
vineetk13 <vineetkme.99@gmail.com>
2020-10-11 00:05:22 +0530

    Updates

---
+++ precache-manifest.2681e26c11c81750de58003c409b423b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dae82be6e3301a757023f721cf50b7",
+    "url": "/sample-deploy-page/index.html"
+  },
+  {
+    "revision": "07f4c0666855f9dd0118",
+    "url": "/sample-deploy-page/static/css/main.d0a6b0c8.chunk.css"
+  },
+  {
+    "revision": "c620b52cf32a0e9c1566",
+    "url": "/sample-deploy-page/static/js/2.af7bb603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-deploy-page/static/js/2.af7bb603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f4c0666855f9dd0118",
+    "url": "/sample-deploy-page/static/js/main.9c94daab.chunk.js"
+  },
+  {
+    "revision": "3923f39e1cf0ac203904",
+    "url": "/sample-deploy-page/static/js/runtime-main.6f4e5352.js"
+  },
+  {
+    "revision": "f44bf297a063d0c52292bddd7d63622d",
+    "url": "/sample-deploy-page/static/media/undraw_react.f44bf297.svg"
+  }
+]);
